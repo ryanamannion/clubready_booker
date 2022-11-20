5ef0cadd424d06812f65703600da10aa95e0904e
ryanamannion <ram321@georgetown.edu>
2022-11-20 15:51:52 -0500

    add stream handler

--- clubready_booker/__init__.py
+++ clubready_booker/__init__.py
@@ -1,11 +1,25 @@
 import logging
+import os
 
 __version__ = "0.0.0-dev"
 
+LOGFILE = "clubready_booker.log"
+LOGLEVEL = os.environ.get("LOGLEVEL", logging.INFO)
+
+file_handler = logging.FileHandler(
+    LOGFILE
+)
+handlers = [file_handler, logging.StreamHandler()]
+
+msg_format = (
+    "%(asctime)s: [%(filename)s:%(lineno)s - %(funcName)s ] "
+    "%(levelname)s - %(message)s"
+)
+
 logging.basicConfig(
-    filename='clubready_booker.log',
-    level=logging.INFO,
-    format="%(levelname)s: %(asctime)s - %(message)s"
+    level=LOGLEVEL,
+    format=msg_format,
+    handlers=handlers
 )
 
 logging.info(f"Running clubready_booker on version {__version__}")
