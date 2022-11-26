323733f755bdf770abf4772c530d3b1c60e107d9
ryanamannion <ram321@georgetown.edu>
2022-11-25 21:42:01 -0500

    add venv stuff

--- cron_job.sh
+++ cron_job.sh
@@ -7,4 +7,10 @@
 # $ crontab -e
 # and enter this to run the script every night at 00:01
 # 1 0 * * 1-7 cron_job.sh
+
+# When you run this script, you will probably want it to run from the top level
+# clubready_booker dir, so CD there and make sure the virtual env is set up
+# cd path/to/toplevel/dir
+# source full/path/to/venv/bin/activate
 python clubready_booker/booker.py
+# deactivate
